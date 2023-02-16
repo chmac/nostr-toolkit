66b6559405f0c6a66b2982899f0f36a7c74d0f5d
Callum Macdonald <git@callum-macdonald.com>
2023-02-16 11:28:07 +0100

    Remove menu anchoring.
    This didn't work. It worked only after a hot reload, but not after a refresh.

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -6,42 +6,15 @@
   import List, { Item, Separator, Text } from "@smui/list";
 
   let menu: Menu;
-  let anchor: HTMLDivElement;
-  let anchorClasses: {
-    [k: string]: boolean;
-  } = {};
 </script>
 
 <TopAppBar variant="fixed">
   <Row>
     <Section>
-      <div
-        class={Object.keys(anchorClasses).join(" ")}
-        use:Anchor={{
-          addClass: (className) => {
-            if (!anchorClasses[className]) {
-              anchorClasses[className] = true;
-            }
-          },
-          removeClass: (className) => {
-            if (anchorClasses[className]) {
-              delete anchorClasses[className];
-              anchorClasses = anchorClasses;
-            }
-          },
-        }}
-        bind:this={anchor}
+      <IconButton class="material-icons" on:click={() => menu.setOpen(true)}
+        >menu</IconButton
       >
-        <IconButton class="material-icons" on:click={() => menu.setOpen(true)}
-          >menu</IconButton
-        >
-      </div>
-      <Menu
-        bind:this={menu}
-        anchor={false}
-        bind:anchorElement={anchor}
-        anchorCorner="BOTTOM_END"
-      >
+      <Menu bind:this={menu}>
         <List>
           <Item>
             <Text>Menu item 1</Text>
