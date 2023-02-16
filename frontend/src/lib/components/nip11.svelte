4c61b13d53997bc0e3d194276f390f62f5ef7d51
Callum Macdonald <git@callum-macdonald.com>
2023-02-16 12:24:50 +0100

    NIP11

---
+++ frontend/src/lib/components/nip11.svelte
@@ -0,0 +1,25 @@
+<script type="ts">
+  import Button from "@smui/button";
+  import Textfield from "@smui/textfield";
+
+  let relayUrl = "";
+  let output = "";
+
+  const getRelayInfo = async () => {
+    const result = await fetch("https://relay.nostr.nu", {
+      headers: { accept: "application/nostr+json" },
+    });
+    const data = await result.json();
+    output = JSON.stringify(data, null, 2);
+  };
+</script>
+
+<h2>NIP11</h2>
+<details>
+  <summary>Fetch NIP11 info for relays</summary>
+  <form on:submit|preventDefault={() => getRelayInfo()}>
+    <Textfield type="url" bind:value={relayUrl} style="width: 100%" />
+    <Button>Fetch relay info</Button>
+  </form>
+  <pre>{output}</pre>
+</details>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
   import Keys from "../lib/components/keys.svelte";
+  import Nip11 from "../lib/components/nip11.svelte";
   import Nip5 from "../lib/components/nip5.svelte";
 </script>
 
 <div class="container">
   <Keys />
-
   <Nip5 />
+  <Nip11 />
 </div>
 
 <style>
